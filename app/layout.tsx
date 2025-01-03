b0458029d8757279d627c80c800868f0a26339f7
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-03 10:29:08 +0530

    Update dependencies and change font in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const dm_sans = DM_Sans({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={dm_sans.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
