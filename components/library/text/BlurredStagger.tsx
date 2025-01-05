924d018fe8113109bfd265495dd25acc5e21392a
preetsuthar17 <preetsutharxd@gmail.com>
2025-01-05 10:21:17 +0530

    Add BlurredStagger component with staggered blur animation; update documentation and meta information

---
+++ components/library/text/BlurredStagger.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const BlurredStagger = ({ text="we love hextaui.com ❤️" }: { text: string }) => {
+  const headingText = text;
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.015,
+      },
+    },
+  };
+
+  const letterAnimation = {
+    hidden: {
+      opacity: 0,
+      filter: "blur(10px)",
+    },
+    show: {
+      opacity: 1,
+      filter: "blur(0px)",
+    },
+  };
+
+  return (
+    <>
+      <div>
+        <motion.h1
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="text-2xl font-semibold tracking-tight text-pretty"
+        >
+          {headingText.split("").map((char, index) => (
+            <motion.span
+              key={index}
+              variants={letterAnimation}
+              transition={{ duration: 0.3 }}
+            >
+              {char === " " ? "\u00A0" : char}
+            </motion.span>
+          ))}
+        </motion.h1>
+      </div>
+    </>
+  );
+};
