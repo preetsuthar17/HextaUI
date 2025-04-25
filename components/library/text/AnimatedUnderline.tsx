08115630332aff02fb975249338dc92480089023
preetsuthar17 <preetsutharxd@gmail.com>
2025-04-25 11:24:58 +0530

    style(text): update color scheme to use primary color in AnimatedUnderline
    The color scheme for the AnimatedUnderline component and its documentation has been updated to use the primary color instead of neutral-400. This change aligns the component with the design system's color palette for better consistency.

--- components/library/text/AnimatedUnderline.tsx
+++ components/library/text/AnimatedUnderline.tsx
@@ -2,8 +2,8 @@
 
 export function AnimatedUnderline() {
   return (
-    <div className="relative after:absolute after:bg-neutral-400 after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">
-      <span className="text-neutral-400">Hover me</span>
+    <div className="relative after:absolute after:bg-primary after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">
+      <span className="text-primary">Hover me</span>
     </div>
   );
 }
