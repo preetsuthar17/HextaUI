d06773d252ffbb82ce728add5a25de3f01488dd8
preetsuthar17 <preetsutharxd@gmail.com>
2025-04-25 12:08:47 +0530

    feat: add GoatCounter analytics script to layout
    The GoatCounter analytics script has been added to the layout to track website traffic. This change enhances the ability to monitor user interactions and gather insights for further improvements.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,14 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={dm_sans.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         {" "}
-        <Banner variant="rainbow">
-          We are working on new components &lt;3
-        </Banner>
+        <Banner variant="rainbow">I'm working on new components &lt;3</Banner>
         <RootProvider>{children}</RootProvider>
         <GoogleAnalytics gaId="G-MYXZQWL3V4" />
+        <script
+          data-goatcounter="https://hextui.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
       </body>
     </html>
   );
