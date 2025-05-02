5c4bd29e4363b352678f81e9bf06538ff097f458
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-02 21:16:58 +0530

    feat(layout): add sponsor link to banner
    Added a `Link` component from `next/link` to the banner, directing users to the HextaUI GitHub sponsors page. This encourages support for the project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { DM_Sans } from "next/font/google";
 import type { ReactNode } from "react";
 import { Banner } from "fumadocs-ui/components/banner";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Link from "next/link";
 
 const dm_sans = DM_Sans({
   subsets: ["latin"],
@@ -14,7 +15,11 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={dm_sans.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         {" "}
-        <Banner variant="rainbow">I'm working on new components &lt;3</Banner>
+        <Banner variant="rainbow">
+          <Link href="https://github.com/sponsors/preetsuthar17">
+            Sponsor HextaUI &lt;3
+          </Link>{" "}
+        </Banner>
         <RootProvider>{children}</RootProvider>
         <GoogleAnalytics gaId="G-MYXZQWL3V4" />
         <script
