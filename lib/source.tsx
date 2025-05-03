85503c6825ac2f56465cb0cbd7ec0097592b477b
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-03 16:23:20 +0530

    refactor: migrate source.ts to source.tsx and extend frontmatter schema
    This commit refactors the source configuration by migrating `source.ts` to `source.tsx` and extending the frontmatter schema to include a `new` boolean field. The changes enhance the file upload documentation by allowing the marking of new features with a visual indicator.

---
+++ lib/source.tsx
@@ -0,0 +1,34 @@
+import { docs, meta } from "@/.source";
+import { createMDXSource } from "fumadocs-mdx";
+import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement } from "react";
+
+export const source = loader({
+  baseUrl: "/docs",
+  source: createMDXSource(docs, meta),
+  pageTree: {
+    attachFile(node, file) {
+      if (!file) return node;
+
+      const data = file.data.data as {
+        new: boolean;
+      };
+      if (data.new)
+        node.name = (
+          <>
+            {node.name}
+            <div className="border-green-600 border bg-green-500/20 text-xs py-0.2 px-2 rounded-full font-medium">
+              New
+            </div>
+          </>
+        );
+
+      return node;
+    },
+  },
+  icon(icon) {
+    if (icon && icon in icons)
+      return createElement(icons[icon as keyof typeof icons]);
+  },
+});
--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,18 @@
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import {
+  defineDocs,
+  defineConfig,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+
+import { z } from "zod";
 
 export const { docs, meta } = defineDocs({
   dir: "content/docs",
+  docs: {
+    schema: frontmatterSchema.extend({
+      new: z.boolean().default(false),
+    }),
+  },
 });
 
 export default defineConfig();
