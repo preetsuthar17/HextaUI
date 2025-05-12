9f6480f08d9e4522ee3d9ec0b8b3b8bf72bf1500
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-12 18:48:17 +0530

    feat(mdx-components): add image zoom functionality to MDX images
    Enhance the MDX components by integrating the `ImageZoom` component from `fumadocs-ui`. This allows users to zoom in on images within MDX content, improving the user experience for detailed image viewing.

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,12 +2,14 @@ import defaultComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { createGenerator } from "fumadocs-typescript";
 import { AutoTypeTable } from "fumadocs-typescript/ui";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 const generator = createGenerator();
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
     AutoTypeTable: (props) => (
       <AutoTypeTable {...props} generator={generator} />
     ),
