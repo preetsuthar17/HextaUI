07f44ca5146866d4e691bce27c19da384c3b7b1c
preetsuthar17 <preetsutharxd@gmail.com>
2025-05-19 14:18:31 +0530

    feat: add JetBrains Mono font to layout and update code styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, JetBrains_Mono } from "next/font/google";
 import type { ReactNode } from "react";
-import { Banner } from "fumadocs-ui/components/banner";
 import { GoogleAnalytics } from "@next/third-parties/google";
+
+import { Banner } from "fumadocs-ui/components/banner";
 import Link from "next/link";
 
 const dm_sans = DM_Sans({
   subsets: ["latin"],
 });
 
+const jetbrains_mono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-code",
+  weight: ["500", "700"],
+});
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={dm_sans.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${dm_sans.className} ${jetbrains_mono.variable}`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         {" "}
         {/* <Banner variant="rainbow">
