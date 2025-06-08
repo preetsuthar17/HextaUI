fc90e4590590474da663e5557b0b5b94d8c6e56b
preetsuthar17 <preetsutharxd@gmail.com>
2025-06-08 07:40:46 +0530

    feat(checkbox): add Checkbox component with examples and documentation

---
+++ src/components/ui/Checkbox/index.ts
@@ -0,0 +1,11 @@
+export { Checkbox, checkboxVariants, type CheckboxProps } from "./checkbox";
+export {
+  CheckboxBasic,
+  CheckboxVariants,
+  CheckboxSizes,
+  CheckboxWithDescription,
+  CheckboxIndeterminate,
+  CheckboxWithError,
+  CheckboxGroup,
+  CheckboxAnimated,
+} from "./CheckboxExamples";
