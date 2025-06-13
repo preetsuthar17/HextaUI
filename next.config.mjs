978b7f824e05553debdf79a4fe271eb2a891a8b3
preetsuthar17 <preetsutharxd@gmail.com>
2025-06-13 14:56:42 +0530

    fix: correct images configuration in Next.js config file

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.imgur.com"],
+  },
 };
 
 export default withMDX(config);
