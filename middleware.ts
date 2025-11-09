5f4a065db707db055d3a7b47f029243905c16fc9
preetsuthar17 <preetsutharxd@gmail.com>
2025-11-09 20:43:09 +0530

    feat: integrate @wandry/analytics-sdk for event tracking and add middleware for request handling

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { captureRegistryEvent } from "@wandry/analytics-sdk";
+
+export function middleware(request: NextRequest) {
+  const token = process.env.WANDRY_ANALYTICS_TOKEN;
+
+  if (token) {
+    captureRegistryEvent(request, token);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+
