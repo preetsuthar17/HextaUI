e05084f0faf9c14c3b76e2d34060bba733a6f80e
preetsuthar17 <preetsutharxd@gmail.com>
2025-11-09 13:07:42 +0530

    feat: add navigation link to Blocks and restructure Tabs layout for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import Link from "next/link";
 import { Hero } from "@/components/hero";
 import AIPreview from "@/components/previews/ai-preview";
 import AuthPreview from "@/components/previews/auth-preview";
 import BillingPreview from "@/components/previews/billing-preview";
 import SettingsPreview from "@/components/previews/settings-preview";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
@@ -14,16 +14,20 @@ export default function Home() {
       <div>
         <Hero />
       </div>
-      <p className="text-center text-muted-foreground">
-        We&apos;re working on new components and blocks. code coming soon!
-      </p>
+
       <Tabs className="flex w-full flex-col gap-12" defaultValue="auth">
-        <TabsList>
-          <TabsTrigger value="auth">Authentication</TabsTrigger>
-          <TabsTrigger value="ai">AI</TabsTrigger>
-          <TabsTrigger value="billing">Billing</TabsTrigger>
-          <TabsTrigger value="settings">Settings</TabsTrigger>
-        </TabsList>
+        <div className="flex items-center justify-between gap-4">
+          <TabsList>
+            <TabsTrigger value="auth">Authentication</TabsTrigger>
+            <TabsTrigger value="ai">AI</TabsTrigger>
+            <TabsTrigger value="billing">Billing</TabsTrigger>
+            <TabsTrigger value="settings">Settings</TabsTrigger>
+          </TabsList>
+          <Link className="underline underline-offset-4" href="/blocks">
+            Check out all blocks
+          </Link>
+        </div>
+
         <TabsContent value="auth">
           <AuthPreview />
         </TabsContent>
