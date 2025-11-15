d4cec0414f76ef7353871d8f166495f9d8e1637e
preetsuthar17 <preetsutharxd@gmail.com>
2025-11-15 09:46:52 +0530

    fix: update script tag in layout for improved loading behavior
    - Changed the script tag from a custom component to a standard HTML <script> element with defer attribute for better loading performance.
    - Removed the strategy attribute to streamline script loading in the layout.tsx file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -161,7 +161,8 @@ export default function RootLayout({
             strategy="lazyOnload"
           />
         )}
-        <Script
+        <script
+          defer
           data-cfasync="false"
           data-color="#FF813F"
           data-description="Support me on Buy me a coffee!"
@@ -172,7 +173,6 @@ export default function RootLayout({
           data-x_margin="18"
           data-y_margin="18"
           src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          strategy="lazyOnload"
         />
       </body>
     </html>
