426913cc63b2072338799d22343c71b4cef504bb
preetsuthar17 <preetsutharxd@gmail.com>
2025-12-13 15:38:51 +0530

    feat: add accordion component and update UI utilities

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -103,7 +103,7 @@ export function TooltipArrow({
   return (
     <BaseTooltip.Arrow
       className={cn(
-        "z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground",
+        "z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground",
         "data-[side=left]:-right-1 data-[side=left]:-translate-y-1/2 data-[side=right]:-left-1 data-[side=right]:-translate-y-1/2 data-[side=top]:-bottom-2.5 data-[side=bottom]:top-1 data-[side=left]:top-1/2! data-[side=right]:top-1/2!",
         className
       )}
